17e285bda188c68631e936497797aacbaf7c5e9f
sakibsarkar <sakibsarkar707@gmail.com>
2023-11-23 18:58:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+require('dotenv').config()
+const express = require("express")
+const port = process.env.PORT || 5000
+const app = express()
+const cors = require("cors")
+const jwt = require("jsonwebtoken")
+
+// middlewere
+app.use(express.json())
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    credentials: true
+}))
+app.use(cookieParser())
+
+
+// token varify
+const varifyToekn = (req, res, next) => {
+    const token = req.cookies.token
+    if (!token) {
+        res.status(401).send({ messege: "unAuthorized Access" })
+        return
+    }
+
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ messege: "Access Forbidden" })
+        }
+        req.user = decoded
+        next()
+    })
+
+}
+
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.xbiw867.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+
+        
+
+        // user token api
+        app.post("/api/user/token", async (req, res) => {
+            const email = req.body
+            const token = jwt.sign(email, process.env.SECRET, { expiresIn: "1h" })
+            res.cookie("token", token, { httpOnly: true, secure: true, sameSite: 'none' }).send({ "messege": "success" })
+        })
+
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.listen(port, () => {
+    console.log("server is running");
+})
