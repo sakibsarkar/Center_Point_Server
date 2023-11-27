7e1a48b6d5225e5fcc49a3b2a09f3215f4c8ac67
sakibsarkar <sakibsarkar707@gmail.com>
2023-11-27 16:19:40 +0600

    live site url added

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const stripe = require("stripe")(process.env.STRIPE)
 // middlewere
 app.use(express.json())
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://assignment-12-65a07.web.app"],
     credentials: true
 }))
 app.use(cookieParser())
